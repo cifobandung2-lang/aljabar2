f6a7ae240248af69f75b49d9d56eed1343455466
cifobandung2-lang <cifobandung2@gmail.com>
2026-05-27 15:29:59 +0700

    tugas aljabar

--- frontend/script.js
+++ frontend/script.js
@@ -23,6 +23,52 @@ async function hitung() {
     hasil = "Soal belum tersedia";
   }
 
+  async function hitung() {
+  let soal = document.getElementById("inputSoal").value;
+  let hasil = "";
+
+  // Hilangkan spasi biar lebih fleksibel
+  soal = soal.replace(/\s/g, "");
+
+  // LIST SOAL ALGEBRA
+  if (soal === "x^2-9") {
+    hasil = "(x - 3)(x + 3)";
+  } 
+  else if (soal === "x^2-4") {
+    hasil = "(x - 2)(x + 2)";
+  } 
+  else if (soal === "x^2-16") {
+    hasil = "(x - 4)(x + 4)";
+  } 
+  else if (soal === "x^2+6x+9") {
+    hasil = "(x + 3)^2";
+  } 
+  else if (soal === "x^2+5x+6") {
+    hasil = "(x + 2)(x + 3)";
+  } 
+  else if (soal === "x^2-5x+6") {
+    hasil = "(x - 2)(x - 3)";
+  } 
+  else if (soal === "2x+3x") {
+    hasil = "5x";
+  } 
+  else if (soal === "4x-2x") {
+    hasil = "2x";
+  } 
+  else if (soal === "3x*2x") {
+    hasil = "6x^2";
+  } 
+  else if (soal === "(x+2)(x+3)") {
+    hasil = "x^2 + 5x + 6";
+  } 
+  else if (soal === "(x-3)(x+3)") {
+    hasil = "x^2 - 9";
+  } 
+  else {
+    hasil = "Soal belum tersedia 😅";
+  }
+
   document.getElementById("hasil").innerText = hasil;
+}
 }
 }
